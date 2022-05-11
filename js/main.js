01696e4ff2dd7050bdde62913b82225c457a007e
TiroZit <nikus.master@gmail.com>
2022-05-11 23:29:38 +0500

    Updates

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 (function() {
   var __webpack_modules__ = {
     537: function() {},
-    870: function(__unused_webpack___webpack_module__, __unused_webpack___webpack_exports__, __webpack_require__) {
+    220: function(__unused_webpack___webpack_module__, __unused_webpack___webpack_exports__, __webpack_require__) {
       "use strict";
       var injectStylesIntoStyleTag = __webpack_require__(379);
       var injectStylesIntoStyleTag_default = __webpack_require__.n(injectStylesIntoStyleTag);
@@ -26,6 +26,102 @@
       injectStylesIntoStyleTag_default()(style_default(), options);
       style_default() && style_default().locals && style_default().locals;
       __webpack_require__(944);
+      function DynamicAdapt(type) {
+        this.type = type;
+      }
+      DynamicAdapt.prototype.init = function() {
+        const _this = this;
+        this.оbjects = [];
+        this.daClassname = "_dynamic_adapt_";
+        this.nodes = document.querySelectorAll("[data-da]");
+        for (let i = 0; i < this.nodes.length; i++) {
+          const node = this.nodes[i];
+          const data = node.dataset.da.trim();
+          const dataArray = data.split(",");
+          const оbject = {};
+          оbject.element = node;
+          оbject.parent = node.parentNode;
+          оbject.destination = document.querySelector(dataArray[0].trim());
+          оbject.breakpoint = dataArray[1] ? dataArray[1].trim() : "767";
+          оbject.place = dataArray[2] ? dataArray[2].trim() : "last";
+          оbject.index = this.indexInParent(оbject.parent, оbject.element);
+          this.оbjects.push(оbject);
+        }
+        this.arraySort(this.оbjects);
+        this.mediaQueries = Array.prototype.map.call(this.оbjects, (function(item) {
+          return "(" + this.type + "-width: " + item.breakpoint + "em)," + item.breakpoint;
+        }), this);
+        this.mediaQueries = Array.prototype.filter.call(this.mediaQueries, (function(item, index, self) {
+          return Array.prototype.indexOf.call(self, item) === index;
+        }));
+        for (let i = 0; i < this.mediaQueries.length; i++) {
+          const media = this.mediaQueries[i];
+          const mediaSplit = String.prototype.split.call(media, ",");
+          const matchMedia = window.matchMedia(mediaSplit[0]);
+          const mediaBreakpoint = mediaSplit[1];
+          const оbjectsFilter = Array.prototype.filter.call(this.оbjects, (function(item) {
+            return item.breakpoint === mediaBreakpoint;
+          }));
+          matchMedia.addListener((function() {
+            _this.mediaHandler(matchMedia, оbjectsFilter);
+          }));
+          this.mediaHandler(matchMedia, оbjectsFilter);
+        }
+      };
+      DynamicAdapt.prototype.mediaHandler = function(matchMedia, оbjects) {
+        if (matchMedia.matches) for (let i = 0; i < оbjects.length; i++) {
+          const оbject = оbjects[i];
+          оbject.index = this.indexInParent(оbject.parent, оbject.element);
+          this.moveTo(оbject.place, оbject.element, оbject.destination);
+        } else for (let i = оbjects.length - 1; i >= 0; i--) {
+          const оbject = оbjects[i];
+          if (оbject.element.classList.contains(this.daClassname)) this.moveBack(оbject.parent, оbject.element, оbject.index);
+        }
+      };
+      DynamicAdapt.prototype.moveTo = function(place, element, destination) {
+        element.classList.add(this.daClassname);
+        if ("last" === place || place >= destination.children.length) {
+          destination.insertAdjacentElement("beforeend", element);
+          return;
+        }
+        if ("first" === place) {
+          destination.insertAdjacentElement("afterbegin", element);
+          return;
+        }
+        destination.children[place].insertAdjacentElement("beforebegin", element);
+      };
+      DynamicAdapt.prototype.moveBack = function(parent, element, index) {
+        element.classList.remove(this.daClassname);
+        if (void 0 !== parent.children[index]) parent.children[index].insertAdjacentElement("beforebegin", element); else parent.insertAdjacentElement("beforeend", element);
+      };
+      DynamicAdapt.prototype.indexInParent = function(parent, element) {
+        const array = Array.prototype.slice.call(parent.children);
+        return Array.prototype.indexOf.call(array, element);
+      };
+      DynamicAdapt.prototype.arraySort = function(arr) {
+        if ("min" === this.type) Array.prototype.sort.call(arr, (function(a, b) {
+          if (a.breakpoint === b.breakpoint) {
+            if (a.place === b.place) return 0;
+            if ("first" === a.place || "last" === b.place) return -1;
+            if ("last" === a.place || "first" === b.place) return 1;
+            return a.place - b.place;
+          }
+          return a.breakpoint - b.breakpoint;
+        })); else {
+          Array.prototype.sort.call(arr, (function(a, b) {
+            if (a.breakpoint === b.breakpoint) {
+              if (a.place === b.place) return 0;
+              if ("first" === a.place || "last" === b.place) return 1;
+              if ("last" === a.place || "first" === b.place) return -1;
+              return b.place - a.place;
+            }
+            return b.breakpoint - a.breakpoint;
+          }));
+          return;
+        }
+      };
+      const da = new DynamicAdapt("max");
+      da.init();
       window["FLS"] = false;
     }
   };
@@ -130,7 +226,7 @@
     __webpack_require__.nc = void 0;
   }();
   var __webpack_exports__ = __webpack_require__.O(void 0, [ 216 ], (function() {
-    return __webpack_require__(870);
+    return __webpack_require__(220);
   }));
   __webpack_exports__ = __webpack_require__.O(__webpack_exports__);
 })();
